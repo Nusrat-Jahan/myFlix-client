c5315c51e658d8483f2c1cfb4e52bb433b55d268
Nusrat-Jahan <jahannusrat735@gmail.com>
2021-04-21 17:07:17 +0200

    Added movies array and then renders them as movie-card

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
+
+class MainView extends React.Component {
+
+  constructor() {
+    // Call the superclass constructor
+    // so React can initialize it
+    super();
+    this.state = {
+      movies: [
+        { _id: 1, Title: 'Silence of the Lambs', Description: 'A young FBI cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer.', ImagePath: 'https://m.media-amazon.com/images/M/MV5BNjNhZTk0ZmEtNjJhMi00YzFlLWE1MmEtYzM1M2ZmMGMwMTU4XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg', Genre: 'Crime', Director: 'Jonathan Demme' },
+        { _id: 2, Title: 'The Shawshank Redemption', Description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', ImagePath: 'https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg', Genre: 'Drama', Director: 'Frank Darabont' },
+        { _id: 3, Title: 'The Godfather', Description: "An organized crime dynasty's aging patriarch transfers control of his clandestine empire to his reluctant son.", ImagePath: 'https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY268_CR3,0,182,268_AL_.jpg', Genre: 'Crime', Director: 'Francis Ford Coppola' }
+      ],
+      selectedMovie: null
+    };
+  }
+
+  setSelectedMovie(newSelectedMovie) {
+    this.setState({
+      selectedMovie: newSelectedMovie
+    });
+  }
+
+  render() {
+    const { movies, selectedMovie } = this.state;
+
+    if (movies.length === 0)
+      return <div className="main-view">The list is empty!</div>;
+
+    return (
+      <div className="main-view">
+        {selectedMovie
+          ? <MovieView movieData={selectedMovie} onBackClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie); }} />
+          : movies.map(movie => (
+            <MovieCard key={movie._id} movieData={movie} onMovieClick={(movieData) => { this.setSelectedMovie(movieData) }} />
+          ))
+        }
+      </div> // movieData is reffered as props
+    );
+  }
+}
+export default MainView;
