d371f332c0a2d6b14bbc1621f3cba7b0af775376
Nusrat-Jahan <jahannusrat735@gmail.com>
2021-04-29 10:29:33 +0200

    Added react bootstrap UI for registration view

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,4 +1,8 @@
 import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import PropTypes from 'prop-types';
 
 export function RegistrationView(props) {
@@ -14,25 +18,32 @@ export function RegistrationView(props) {
   };
 
   return (
-    <form>
-      <label>
-        Username:
-        <input type='text' value={username} onChange={e => setUsername(e.target.value)} />
-      </label>
-      <label>
-        Password:
-        <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
-      </label>
-      <label>
-        Email:
-        <input type='text' value={email} onChange={e => setEmail(e.target.value)} />
-      </label>
-      <label>
-        Birthdate:
-        <input type='text' value={birthdate} onChange={e => setBirthdate(e.target.value)} />
-      </label>
-      <button type='submit' onClick={handleSubmit}>Submit</button>
-    </form>
+    <Row className="login-form justify-content-md-center">
+      <Col md={6}>
+        <Form>
+          <h1 className="text-primary">Welcome to MyFlix!</h1>
+          <p className="text-secondary">Please Register to continue</p>
+          <Form.Group controlId="formUsername">
+            <Form.Label>Username:</Form.Label>
+            <Form.Control type="text" placeholder='Enter Username' onChange={e => setUsername(e.target.value)} />
+          </Form.Group>
+          <Form.Group controlId="formPassword">
+            <Form.Label>Password:</Form.Label>
+            <Form.Control type="password" placeholder='Enter Password' onChange={e => setPassword(e.target.value)} />
+            <p className="text-secondary">Note: Do not share your password with anyone.</p>
+          </Form.Group>
+          <Form.Group controlId="formEmail">
+            <Form.Label>Email:</Form.Label>
+            <Form.Control type="text" placeholder='Enter your Email' onChange={e => setEmail(e.target.value)} />
+          </Form.Group>
+          <Form.Group controlId="formBirthdate">
+            <Form.Label>Birthdate:</Form.Label>
+            <Form.Control type="text" placeholder='Enter your Birthday' onChange={e => setBirthdate(e.target.value)} />
+          </Form.Group>
+          <Button type='submit' onClick={handleSubmit} block>Submit</Button>
+        </Form>
+      </Col>
+    </Row>
   )
 }
 
