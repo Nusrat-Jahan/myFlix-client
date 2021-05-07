f352d167cab17dbfea9ac90c57924f37711a9979
Nusrat-Jahan <jahannusrat735@gmail.com>
2021-05-07 13:35:30 +0200

    Added link to the director and genre view

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,25 +1,31 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import CardGroup from 'react-bootstrap/CardGroup'
-
 
 export class MovieCard extends React.Component {
   render() {
-    const { movieData, onMovieClick } = this.props;
+    const { movieData } = this.props;
     // return <div className="movie-card" onClick={() => { onMovieClick(movieData); }}>{movieData.Title}</div>;
     return (
-      <CardGroup>
-        <Card>
-          <Card.Img variant="top" src={movieData.ImagePath} />
-          <Card.Body>
-            <Card.Title>{movieData.Title}</Card.Title>
-            <Card.Text>{movieData.Description}</Card.Text>
-            <Button block onClick={() => onMovieClick(movieData)} variant="primary">Open</Button>
-          </Card.Body>
-        </Card>
-      </CardGroup>
+      <Card>
+        <Card.Img variant="top" src={movieData.ImagePath} />
+        <Card.Body>
+          <Card.Title>{movieData.Title}</Card.Title>
+          <Card.Text>{movieData.Description}</Card.Text>
+          <Link to={`/movies/${movieData._id}`}>
+            <Button className="mb-2" block variant="primary">Open</Button>
+          </Link>
+          <Link to={`/directors/${movieData.Director.Name}`}>
+            <Button className="mb-2" block variant="primary">Director</Button>
+          </Link>
+
+          <Link to={`/genres/${movieData.Genre.Name}`}>
+            <Button className="mb-2" block variant="primary">Genre</Button>
+          </Link>
+        </Card.Body>
+      </Card>
     )
   }
 }
@@ -27,19 +33,18 @@ export class MovieCard extends React.Component {
 MovieCard.propTypes = {
   movieData: PropTypes.shape({
     Title: PropTypes.string,
-    Description: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired
     // ImagePath: PropTypes.arrayOf(PropTypes.string).isRequired,
-    ImagePath: PropTypes.string.isRequired,
-    Genre: PropTypes.shape({
-      Name: PropTypes.string.isRequired,
-      Description: PropTypes.string.isRequired
-    }),
-    Director: PropTypes.shape({
-      Name: PropTypes.string.isRequired,
-      Bio: PropTypes.string.isRequired,
-      Birth: PropTypes.string.isRequired,
-      Death: PropTypes.string,
-    }),
+    //   ImagePath: PropTypes.string.isRequired,
+    //   Genre: PropTypes.shape({
+    //     Name: PropTypes.string.isRequired,
+    //     Description: PropTypes.string.isRequired
+    //   }),
+    //   Director: PropTypes.shape({
+    //     Name: PropTypes.string.isRequired,
+    //     Bio: PropTypes.string.isRequired,
+    //     Birth: PropTypes.string.isRequired,
+    //     Death: PropTypes.string,
+    //   }),
   }).isRequired,
-  onMovieClick: PropTypes.func.isRequired
 };
