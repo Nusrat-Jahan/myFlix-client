33a954e97b101fb77b90d9f87bb64e6fef7ac06e
Nusrat-Jahan <jahannusrat735@gmail.com>
2021-05-09 20:01:58 +0200

    Added add favorite movie button on movie card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import axios from "axios";
 import { Link } from "react-router-dom";
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 export class MovieCard extends React.Component {
+  handleAdd() {
+    const token = localStorage.getItem("token");
+    const user = localStorage.getItem("user");
+    axios.post(`https://myflix-movie-app.herokuapp.com/users/${user}` + "/movies/" +
+      this.props.movieData._id, {},
+      { headers: { Authorization: `Bearer ${token}` } }
+    )
+      .then((response) => {
+        console.log(response);
+        alert(this.props.movieData.Title + " has been added to your favorites movie.");
+      })
+  }
   render() {
     const { movieData } = this.props;
     // return <div className="movie-card" onClick={() => { onMovieClick(movieData); }}>{movieData.Title}</div>;
@@ -23,6 +36,9 @@ export class MovieCard extends React.Component {
 
           <Link to={`/genres/${movieData.Genre.Name}`}>
             <Button className="mb-2" block variant="primary">Genre</Button>
+          </Link>
+          <Link to={`/movies/${movieData._id}`}>
+            <Button className="mb-2" block variant="primary" onClick={() => this.handleAdd(movieData)}>Add to favourite</Button>
           </Link>
         </Card.Body>
       </Card>
